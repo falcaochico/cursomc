f03d22de2ded054464a132cf243a8daf60fc81cb
Francisco Falcão <falcaofranciscojr@gmail.com>
2020-09-20 13:01:44 -0300

    Tesando a primeira classe de domínio - Categoria

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.nelioalves.cursomc.resources;
 
+import com.nelioalves.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST funcionando";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> categorias = new ArrayList<>();
+        categorias.addAll(Arrays.asList(cat1, cat2));
+
+        return categorias;
     }
 }
