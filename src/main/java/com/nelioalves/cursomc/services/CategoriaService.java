6310b45d07c130545d95cad13682865479f82c31
falcaochico <falcaofranciscojr@gmail.com>
2020-09-29 07:32:41 -0300

    Inserindo uma nova Categoria

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -17,4 +17,9 @@ public class CategoriaService {
                Categoria.class.getName())
         );
     }
+
+    public Categoria insert(Categoria obj){
+        obj.setId(null);
+        return repo.save(obj);
+    }
 }
