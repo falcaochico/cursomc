e49831607491d3c899ef38daeb52d8b0735b68aa
falcaochico <falcaofranciscojr@gmail.com>
2020-10-10 12:11:02 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/nelioalves/cursomc/services/ProdutoService.java
@@ -0,0 +1,36 @@
+package com.nelioalves.cursomc.services;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Produto;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.repositories.ProdutoRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    @Autowired private ProdutoRepository repo;
+    @Autowired private CategoriaRepository categoriaRepository;
+
+    public Produto find(Integer id){
+        return repo.findById(id).orElseThrow(()->
+           new ObjectNotFoundException("Objeto não encontrado! Id: "+id+", Tipo: "+
+               Produto.class.getName())
+        );
+    }
+
+    public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy,
+                                String direction){
+        PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction),orderBy);
+        List<Categoria> categorias = categoriaRepository.findAllById(ids);
+
+        return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+    }
+}
