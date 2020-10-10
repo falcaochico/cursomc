260e04beebcc84e03cf729e18064e9588e07fdc5
falcaochico <falcaofranciscojr@gmail.com>
2020-10-10 10:05:20 -0300

    validação customizada: email não repetido na atualização de Cliente

---
+++ src/main/java/com/nelioalves/cursomc/services/validations/ClienteUpdateValidator.java
@@ -0,0 +1,51 @@
+package com.nelioalves.cursomc.services.validations;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.domain.enums.TipoCliente;
+import com.nelioalves.cursomc.dto.ClienteDTO;
+import com.nelioalves.cursomc.dto.ClienteNewDTO;
+import com.nelioalves.cursomc.repositories.ClienteRepository;
+import com.nelioalves.cursomc.resources.exceptions.FieldMessage;
+import com.nelioalves.cursomc.services.validations.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Override
+    public void initialize(ClienteUpdate clienteInsert) {
+    }
+
+    @Override
+    public boolean isValid(ClienteDTO clienteDTO, ConstraintValidatorContext constraintValidatorContext) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        Integer uriId = Integer.valueOf(map.get("id"));
+
+        Cliente cliente = clienteRepository.findByEmail(clienteDTO.getEmail());
+        if(cliente != null && !cliente.getId().equals(uriId)){
+            list.add(new FieldMessage("email","O Email já existe"));
+        }
+
+        for (FieldMessage e : list) {
+            constraintValidatorContext.disableDefaultConstraintViolation();
+            constraintValidatorContext.buildConstraintViolationWithTemplate(e.getMessage()).
+                    addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
